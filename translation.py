bdb330fea4e40348cc2534776b71b463e663e0d0
JOHNWICK049 <86424955+JOHNWICK049@users.noreply.github.com>
2021-06-29 11:58:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,22 +1,22 @@
 class Translation(object):
-    START_TEXT = """ <b> Hi {} , I'm Mr File renamer. - I can help you to rename your files I also  help you to convert your file into video with permanent thumbnail support . </b> \n
+    START_TEXT = """ <b> Hi {} , I'm I ❤ MOVIES File renamer. - I can help you to rename your files I also  help you to convert your file into video with permanent thumbnail support . </b> \n
 
-<b>Bot Maintained By: @DevelopedBots </b> \n 
+<b>Bot Maintained By: I ❤ MOVIES Owner </b> \n 
 <b> I can also download & upload files from Zee5</b> \n
 <b>Do /help for more Details ...</b> \n
 """
 
     RENAME_403_ERR = "What Are You Doing? Fool!!"
-    BANNED_USER_TEXT = "Sorry!! But My Owner has Banned You From Using This Service. That Means You Can't Run Me Now! \n \n Contact : @DevelopedBotz For more Details.. " 
+    BANNED_USER_TEXT = "Sorry!! But My Owner has Banned You From Using This Service. That Means You Can't Run Me Now! \n \n Contact : @I4Movies_Office8372_Bot For more Details.. " 
     ABS_TEXT = "What Are You Trying To Do,Mate?"
     UPGRADE_TEXT = "CONTACT @kunaldiwan "
     DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
     UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel @DevelopedBots</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel I ❤ MOVIES</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
-    NOT_AUTH_USER_TEXT = "CONTACT @DevelopedBotz "
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @DevelopedBotz "
+    NOT_AUTH_USER_TEXT = "CONTACT @I4Movies_Office8372_Bot "
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @I4Movies_Office8372_Bot "
     SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
     DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
@@ -28,12 +28,12 @@ class Translation(object):
     NO_THUMB_FOUND = "No Thumbnail found"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>ABOUT ME : \n AM AN TELEGRAM SIMPLE FILE RENAMER BOT. \n YOU CAN RENAME TELEGRAM FILES WITH PERMANANT THUMBNAIL SUPPORT ON ME.\n \n ALSO CONVERT FILES TO VIDEOS \n Feedback @DevelopedBotz If You Found ME Useful</b>"""
+    ABOUT_ME = """<b>ABOUT ME : \n AM AN TELEGRAM SIMPLE FILE RENAMER BOT. \n YOU CAN RENAME TELEGRAM FILES WITH PERMANANT THUMBNAIL SUPPORT ON ME.\n \n ALSO CONVERT FILES TO VIDEOS \n Feedback @I4Movies_Office8372_Bot If You Found ME Useful</b>"""
     HELP_USER = """DO YOU NEED HELP. NO NEED OF ANY HELP HERE . YOU HAVE ANY DOUBTS ASK AT SUPPORT GROUP AND ALL COMMANDS ARE HERE. 🤗🤭
     
 1.🔹 <b>Send me any Telegram File.</b> \n
 2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
 3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
 4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n
-5.🔹 <b> Send Me Any Zee5 Url And I'll Upload It Into Telegram </b> \n \n <b>Made With Love By: @DevelopedBotz </b>"""
+5.🔹 <b> Send Me Any Zee5 Url And I'll Upload It Into Telegram </b> \n \n <b>Made With Love By: I ❤ MOVIES</b>"""
 
